db4d937f7a859b2ce57600026bc00e7959671e29
Donguapo <chamkhianas@gmail.com>
2019-08-25 20:00:39 +0100

    CampusSection+InscrisSection+navbarSection+footerSection

--- greenchip/src/app/app.module.ts
+++ greenchip/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { SecondSectionComponent } from './second-section/second-section.component';
 import { FormationSectionComponent } from './formation-section/formation-section.component';
+import { InscrisSectionComponent } from './inscris-section/inscris-section.component';
+import { CampusSectionComponent } from './campus-section/campus-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroSectionComponent,
     SecondSectionComponent,
-    FormationSectionComponent
+    FormationSectionComponent,
+    InscrisSectionComponent,
+    CampusSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ greenchip/src/app/campus-section/campus-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campus-section',
+  templateUrl: './campus-section.component.html',
+  styleUrls: ['./campus-section.component.css']
+})
+export class CampusSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
